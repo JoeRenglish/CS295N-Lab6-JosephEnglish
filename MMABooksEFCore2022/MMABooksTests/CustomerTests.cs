957b80943b499995af4a1aba8553011f418b6382
Joseph English <work@Josephs-MacBook-Pro.local>
2024-11-24 17:07:00 -0800

    added client

---
+++ MMABooksEFCore2022/MMABooksTests/CustomerTests.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+using NUnit.Framework;
+using MMABooksEFClasses.MarisModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        
+        MMABooksContext dbContext;
+        Customer? c;
+        Invoices? i;
+        List<Customer>? customers;
+        List<Invoices>? invoices;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            dbContext = new MMABooksContext();
+            dbContext.Database.ExecuteSqlRaw("call usp_testingResetData()");
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            customers = dbContext.Customers.OrderBy(c => c.Name).ToList();
+            Assert.AreEqual(696, customers.Count);
+            Assert.AreEqual("Abeyatunge, Derek", customers[0].Name);
+            PrintAll(customers);
+        }
+
+        [Test]
+        public void GetByPrimaryKeyTest()
+        {
+            c = dbContext.Customers.Find(1);
+            Assert.IsNotNull(c);
+            Assert.AreEqual("Molunguri, A", c.Name);
+            Console.WriteLine(c);
+        }
+
+        [Test]
+        public void GetUsingWhere()
+        {
+            // get a list of all of the customers who live in OR
+            customers = dbContext.Customers.Where(c => c.StateCode.Equals("OR")).OrderBy(c => c.Name).ToList();
+            Assert.AreEqual(5, customers.Count);
+            Assert.AreEqual("Erpenbach, Lee", customers[0].Name);
+        }
+
+        [Test]
+        public void GetWithInvoicesTest()
+        {
+            // get the customer whose id is 20 and all of the invoices for that customer
+            c = dbContext.Customers.Find(20);
+            invoices = dbContext.Invoices.Where(i => i.CustomerId.Equals(c.CustomerId)).OrderBy(i => i.InvoiceDate).ToList();
+            Assert.AreEqual(3, invoices.Count);
+            Assert.AreEqual("Chamberland, Sarah", c.Name);
+        }
+
+        [Test]
+        public void GetWithJoinTest()
+        {
+            // get a list of objects that include the customer id, name, statecode and statename
+            var customers = dbContext.Customers.Join(
+               dbContext.States,
+               c => c.StateCode,
+               s => s.StateCode,
+               (c, s) => new { c.CustomerId, c.Name, c.State, s.StateName }).OrderBy(r => r.StateName).ToList();
+            Assert.AreEqual(696, customers.Count);
+            // I wouldn't normally print here but this lets you see what each object looks like
+            foreach (var c in customers)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+            c = dbContext.Customers.Find(1);
+            dbContext.Customers.Remove(c);
+            dbContext.SaveChanges();
+            Assert.IsNull(dbContext.Customers.Find(1));
+        }
+
+        [Test]
+        public void CreateTest()
+        {
+            c = new Customer();
+            c.StateCode = "OR";
+            c.Name = "??????";
+            c.Address = "??????";
+            c.City = "?????";
+            c.ZipCode = "?????";
+            dbContext.Customers.Add(c);
+            dbContext.SaveChanges();
+            Assert.IsNotNull(dbContext.Customers.Find(1));
+
+        }
+
+        [Test]
+        public void UpdateTest()
+        {
+            c = dbContext.Customers.Find(1);
+            c.Name = "???";
+            dbContext.Customers.Update(c);
+            dbContext.SaveChanges();
+            c = dbContext.Customers.Find(1);
+            Assert.AreEqual("???", c.Name);
+
+        }
+
+        public void PrintAll(List<Customer> customers)
+        {
+            foreach (Customer c in customers)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        
+    }
+}
